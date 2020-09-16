72bd0c93d8b19c24b5b2ca31181b8d814cf27822
srikanta gowda <srikantagowda07@gmail.com>
2020-09-16 21:39:42 +0530

    created app.js app. test.js file

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import LandingPage  from "./landing_page/landing_page"
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom'
+import Search from './search_result/seacrh';
+import Write_review from './Write_review/Write_review';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        
+        <Route path='/search' component={Search} />
+          <Route exact path='/' component={LandingPage} />
+          <Route path='/writereview' component={Write_review} />
+      </Router>
+    )
+  }
+}
+
+export default App
